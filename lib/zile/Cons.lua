909a22509fc92637b16a8f10951525f317a36af9
Gary V. Vaughan <gary@gnu.org>
2014-01-13 12:44:59 +1300

    cons: add nice stringification, with '.' notation.
    * lib/zile/Cons.lua: Reorganise somewhat for better LDocs, and
    overall readability.
    (stringify): New function. Return a short string representation
    of a cons list, eliding nested parentheses for cons lists, and
    using '.' notation for non-cons cdr cells.
    (metatable.__tostring): Wrap `stringify` results in parentheses
    and return.
    (metatable._type): Set this to "Cons" for all objects constructed
    by this file.
    (consp): New function. Recognise cons objects by value of the
    metatable._type value.
    
    Signed-off-by: Gary V. Vaughan <gary@gnu.org>

--- lib/zile/Cons.lua
+++ lib/zile/Cons.lua
@@ -52,90 +52,156 @@
 
 local Cons -- forward declaration
 
-
-local methods = {
-  --- Return the nth element of a cons list.
-  -- @function nth
-  -- @tparam number n 1-based index into the list
-  -- @return car element of the nth cell in the cons list
-  nth = function (self, n)
-    if type (n) ~= "number" or n < 1 or self == nil then
-      return nil
-    elseif n == 1 then
-      return self.car
-    end
-
-    return self.nth (self.cdr, n - 1)
-  end,
+------
+-- Cons cell.
+-- @table cons
+-- @field car content address register
+-- @field cdr content decrement register
 
 
-  --- Concantenate the value field of each car element in a list.
-  -- Equivalent to table.concat for lists of cons cells.
-  -- @function concat
-  -- @tparam[opt=""] string delim delimiter to place between elements
-  -- @treturn string `delim` delimited concatenation of the list.
-  concat = function (self, delim)
-    delim = delim or ""
-    local s = tostring (self.car.value or self.car)
-    if self.cdr == nil then return s end
-    return s .. delim .. self.concat (self.cdr, delim)
-  end,
+--- Concantenate the value field of each car element in a list.
+-- Equivalent to table.concat for lists of cons cells.
+-- @tparam[opt=""] string delim delimiter to place between elements
+-- @treturn string `delim` delimited concatenation of the list.
+local function concat (self, delim)
+  delim = delim or ""
+  local s = tostring (self.car.value or self.car)
+  if self.cdr == nil then return s end
+  return s .. delim .. self.concat (self.cdr, delim)
+end
 
 
-  --- Return a new Cons list with elements that match PREDICATE.
-  -- @function filter
-  -- @func predicate a one parameter function that returns a boolean
-  -- @treturn Cons list of elements where PREDICATE returned `true`
-  filter = function (self, predicate)
-    if self == nil then return nil end
+--- Return a new Cons list with elements that match `predicate`.
+-- @func predicate a one parameter function that returns a boolean
+-- @treturn cons list of elements where `predicate` returned `true`
+local function filter (self, predicate)
+  if self == nil then return nil end
 
-    local car = self.car.value or self.car
-    if predicate (car) then
-      return Cons (car, self.filter (self.cdr, predicate))
-    else
-      return self.filter (self.cdr, predicate)
-    end
-  end,
-
-
-  --- Apply a function to the value field of each car element in a list.
-  -- @function mapconcat
-  -- @func func a one parameter function that returns a string
-  -- @tparam[opt=""] string delim delimiter to place between elements
-  -- @treturn string `delim` delimited concatenation of the list.
-  mapconcat = function (self, func, delim)
-    delim = delim or ""
-    local s = tostring (func (self.car.value or self.car))
-    if self.cdr == nil then return s end
-    return s .. delim .. self.mapconcat (self.cdr, func, delim)
-  end,
+  local car = self.car.value or self.car
+  if predicate (car) then
+    return Cons (car, self.filter (self.cdr, predicate))
+  else
+    return self.filter (self.cdr, predicate)
+  end
+end
 
 
-  --- Return a non-destructively reversed cons list.
-  -- @function reverse
-  -- @treturn Cons a new list with elements in reverse order
-  reverse = function (self)
-    local r = nil
-    while self ~= nil do
-      r = Cons (self.car, r)
-      self = self.cdr
-    end
-    return r
+--- Apply a function to the value field of each car element in a list.
+-- @func func a one parameter function that returns a string
+-- @tparam[opt=""] string delim delimiter to place between elements
+-- @treturn string `delim` delimited concatenation of the list.
+local function mapconcat (self, func, delim)
+  delim = delim or ""
+  local s = tostring (func (self.car.value or self.car))
+  if self.cdr == nil then return s end
+  return s .. delim .. self.mapconcat (self.cdr, func, delim)
+end
+
+
+--- Return the nth element of a cons list.
+-- @tparam number n 1-based index into the list
+-- @return car element of the nth cell in the cons list
+local function nth (self, n)
+  if type (n) ~= "number" or n < 1 or self == nil then
+    return nil
+  elseif n == 1 then
+    return self.car
+  end
+
+  return self.nth (self.cdr, n - 1)
+end
+
+
+--- Return a non-destructively reversed cons list.
+-- @treturn cons a new list with elements in reverse order
+local function reverse (self)
+  local r = nil
+  while self ~= nil do
+    r = Cons (self.car, r)
+    self = self.cdr
+  end
+  return r
+end
+
+
+--- Is `x` a Cons object?
+-- @param x a Lua object
+-- @return `true` if `x` is a Cons object, or else `false`
+local function consp (x)
+  return (getmetatable (x) or {})._type == "Cons"
+end
+
+
+
+--- Return a short string representation of a cons list.
+-- @tparam cons cons a cons cell, or cons list
+-- @treturn string a short string representation of `cons`
+local function stringify (cons)
+  if not consp (cons) then return tostring (cons) end
+
+  local s = ""
+  if cons.car ~= nil then s = tostring (cons.car) end
+
+  if consp (cons.cdr) then
+    s = s .. " " .. stringify (cons.cdr)
+  elseif cons.cdr ~= nil then
+    s = s .. " . " .. stringify (cons.cdr)
+  end
+  return s
+end
+
+
+-- Shared metatable for Cons objects.
+-- @table metatable
+-- @string _type used by consp to recognise Cons objects
+-- @tfield table __index a table of object methods
+-- @func __tostring stringification metamethod
+local metatable = {
+  _type = "Cons",
+
+  -- Object methods:
+  __index = {
+    concat    = concat,
+    filter    = filter,
+    mapconcat = mapconcat,
+    nth       = nth,
+    reverse   = reverse,
+  },
+
+  __tostring = function (self)
+    return "(" .. stringify (self) .. ")"
   end,
 }
 
 
+--- Return a new cons cell with supplied car and cdr.
+-- @local
+-- @param car first element
+-- @param cdr last element
+-- @treturn cons a new cell containing those elements
 function Cons (car, cdr)
-  return setmetatable ({car = car, cdr = cdr}, {__index = methods})
+  return setmetatable ({car = car, cdr = cdr}, metatable)
 end
 
 
---- Return a new Cons cell with supplied car and cdr.
--- @function __call
--- @param car first element
--- @param cdr last element
--- @treturn Cons a new cell containing those elements
-return setmetatable (methods, {
+--- @export
+-- Module functions:
+local functions = {
+  concat    = concat,
+  consp     = consp,
+  filter    = filter,
+  mapconcat = mapconcat,
+  nth       = nth,
+  reverse   = reverse,
+}
+
+
+return setmetatable (functions, {
+  --- Return a new Cons cell with supplied car and cdr.
+  -- @function __call
+  -- @param car first element
+  -- @param cdr last element
+  -- @treturn cons a new cell containing those elements
   __call = function (_, ...)
     return Cons (...)
   end,
