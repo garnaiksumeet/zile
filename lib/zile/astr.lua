6f01d0a5bada7f749b30305a897a2584631fe1f1
Gary V. Vaughan <gary@gnu.org>
2014-01-22 12:19:54 +1300

    maint: move `require "alien"` to locality of only use.
    * bin/zmacs, bin/zz: Remove `require "alien"`.
    * lib/zile/astr.lua: Add `require "alien"`.
    
    Signed-off-by: Gary V. Vaughan <gary@gnu.org>

--- lib/zile/astr.lua
+++ lib/zile/astr.lua
@@ -17,6 +17,7 @@
 -- You should have received a copy of the GNU General Public License
 -- along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+local alien  = require "alien"
 local Object = require "std.object"
 
 alien.default.memchr:types ("pointer", "pointer", "int", "size_t")
